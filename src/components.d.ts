8d22be5be5cc496d4f94478cbd2135226cddca3b
Matt Coleman <matt.coleman@protonmail.com>
2025-04-25 16:12:05 +0100

    chore: added deploy.yml to deploy to GH pages on push

--- src/components.d.ts
+++ src/components.d.ts
@@ -9,7 +9,6 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     AppFooter: typeof import('./components/AppFooter.vue')['default']
-    HelloWorld: typeof import('./components/HelloWorld.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     ToDoContainer: typeof import('./components/ToDoContainer.vue')['default']
